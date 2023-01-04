c680778fda5766107adc164ae0d26611c3b36ad2
Thang Le <lctzz540@outlook.com>
2023-01-04 20:09:16 +0700

    add cors middleware

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	"lctzz540/routes"
 	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.New()
 	router.Use(gin.Logger())
+	router.Use(cors.Default())
 	routes.AuthRoutes(router)
 	routes.QuestionRoutes(router)
 	router.GET("/", func(ctx *gin.Context) {
